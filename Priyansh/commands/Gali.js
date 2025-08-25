456fb9638fce62f3386e761e177d6e81be356191
Alishehzada910 <alishehzada910@gmail.com>
2025-08-25 19:57:43 +0500

    Update Gali.js

--- Priyansh/commands/Gali.js
+++ Priyansh/commands/Gali.js
@@ -1,1 +1,21 @@
+module.exports.config = {
+    name: "Gali",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    description: "War In Chatbox",
+    commandCategory: "wargroup",
+    usages: "[noprefix]",
+    cooldowns: 50,
+    dependencies: {
+        "fs-extra": "",
+        "axios": ""
+    }
+}
 
+module.exports.run = async function({ api, args, Users, event}) {
+ var mention = Object.keys(event.mentions)[0];
+    
+ let name =  event.mentions[mention];
+    var arraytag = [];
+        arraytag.push({id: mention});
